21021a5502544057c8521ef980aded57e2c7ba25
esdrascosta <esdras.cts@outlook.com>
2022-09-02 21:32:01 -0300

    [ADD] Customizando as mensagens do Identity para pt-br

--- src/services/Identidade.API/Program.cs
+++ src/services/Identidade.API/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddRoles<IdentityRole>()
+    .AddErrorDescriber<IdentityMensagensPortugues>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
