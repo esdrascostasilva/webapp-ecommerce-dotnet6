e01ea69a75d33af52365a1914985d31593a7b777
esdrascosta <esdras.cts@outlook.com>
2022-09-21 13:53:17 -0300

    [ADD] Consumindo a API de Identidade via HttpService.

--- src/web/LojaWebApp.MVC/Program.cs
+++ src/web/LojaWebApp.MVC/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityConfiguration();
 builder.Services.AddMvcConfiguration();
-
+builder.Services.RegisterService();
 
 var app = builder.Build();
 
