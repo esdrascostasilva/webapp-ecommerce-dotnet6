b2c9bba8442e4485444488a2ef65ae7f597d5097
esdrascosta <esdras.cts@outlook.com>
2023-03-05 07:33:48 -0300

    [ADD] Iniciando o conceito do CQRS: Command e Command Handler

---
+++ src/services/Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Cliente.API.Application.Commands
+{
+	public class ClienteCommandHandler
+	{
+		public void Manipular(RegistrarClienteCommand message)
+		{
+			// validar comando
+
+			// persistir na base
+		}
+	}
+}
+
